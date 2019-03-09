178052ceab12714c1519190b77d6fb554763fd64
mkzarra <mkzarra@gmail.com>
2019-03-09 11:10:00 -0500

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -21,6 +21,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
+      proxy: true,
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     (accessToken, refreshToken, profile, done) => {
