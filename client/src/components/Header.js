833a80a75cef23c0137e3e363a4f1805b79ad529
mkzarra <mkzarra@gmail.com>
2019-03-13 18:03:30 -0400

    Stripe set up completed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
-      case null: return "...loading";
+      case null: return;
       case false: return <li><a href="/auth/google">Login With Google</a></li>;
-      default: return <li><a href="/api/logout">Logout</a></li>;
+      default: return [
+        <li key="1"><Payments /></li>,
+        <li key="2"><a href="/api/logout">Logout</a></li>
+      ];
     }
   }
 
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,11 +2,9 @@ import * as types from '../actions/types';
 // import { fetchUser } from '../actions/index';
 // import { updateObject } from '../shared/utility';
 
-const reducer = function (state = null, action) {
+export default function (state = null, action) {
   switch (action.type) {
     case types.FETCH_USER: return action.payload || false;
     default: return state;
   }
 }
-
-export default reducer;
