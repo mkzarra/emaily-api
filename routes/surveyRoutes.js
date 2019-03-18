19184a45d0ed51f8e38500a5752be0e3917f2026
mkzarra <mkzarra@gmail.com>
2019-03-18 11:22:36 -0400

    surveys can be mailed out using sendgrid library

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
 
 const Survey = mongoose.model('surveys');
 
-module.exports = app => {
+module.exports = async app => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thank you for your feedback!');
+  })
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
@@ -16,5 +20,18 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 }
